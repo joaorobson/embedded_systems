0c63d247ac347c70858fd2ecc82863a9f9cd9c42
Vinicius Lima <vfbdelima@gmail.com>
2020-12-14 09:26:29 -0300

    Added class to send data using mqtt
    Signed-off-by: Vinicius Lima <vfbdelima@gmail.com>

--- trabalho_final/central_server/main.py
+++ trabalho_final/central_server/main.py
@@ -1,7 +1,9 @@
+import json
 import signal
 from threading import Thread, Condition, Lock
 from bme280_sensor import BME280
 from gpio import InputOutputDevices
+from emitter import Emitter
 
 run_bme280 = False
 lock_bme280 = Lock()
@@ -9,6 +11,7 @@ condition_bme280 = Condition(lock_bme280)
 
 run_send_data = False
 lock_send_data = Lock()
+condition_send_data = Condition(lock_send_data)
 
 run_read_security_sensors = False
 lock_read_security_sensors = Lock()
@@ -36,6 +39,15 @@ def read_bme280_sensor(params, state):
         run_bme280 = False
     lock_bme280.release()
 
+def send_data(params, state):
+    lock_send_data.acquire()
+    global run_send_data
+    while not run_send_data:
+        condition_send_data.wait()
+        params.send_data("fse/150154003/central", json.dumps(state))
+        run_send_data = False
+    lock_send_data.release()
+
 def alarm_handler(signum, frame):
     global number_of_executions
     number_of_executions += 1
@@ -47,7 +59,7 @@ def alarm_handler(signum, frame):
         condition_read_security_sensors.notify()
     lock_read_security_sensors.release()
 
-    # reads bme280 every 1 second
+    # reads bme280 and send data every 1 second
     if number_of_executions == 5:
         lock_bme280.acquire()
         global run_bme280
@@ -55,6 +67,13 @@ def alarm_handler(signum, frame):
             run_bme280 = True
             condition_bme280.notify()
         lock_bme280.release()
+
+        lock_send_data.acquire()
+        global run_send_data
+        if not run_send_data:
+            run_send_data = True
+            condition_send_data.notify()
+        lock_send_data.release()
         number_of_executions = 0
 
 if __name__ == '__main__':
@@ -64,9 +83,12 @@ if __name__ == '__main__':
     state = {'bme280': {}, 'devices': {}}
     sensor_bme280 = BME280(state)
     devices = InputOutputDevices(state)
+    emitter = Emitter()
 
     check_security_sensors = Thread(target=read_security_sensors, args=(devices, state,))
     read_temperature_humidity = Thread(target=read_bme280_sensor, args=(sensor_bme280, state,))
+    send_data_thread = Thread(target=send_data, args=(emitter, state,))
 
     check_security_sensors.start()
     read_temperature_humidity.start()
+    send_data_thread.start()
